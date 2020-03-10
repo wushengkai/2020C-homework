c08390ced3c9feadc35880c77507bd47d6bf7fb9
wushengkai <992372558@qq.com>
2020-03-10 16:53:55 +0800

    201910420221-吴晟楷

---
+++ 基础班作业/201920420221 吴晟楷（3）/第一题.cpp
@@ -0,0 +1,11 @@
+
+#include<iostream>
+using namespace std;
+const int build(12);
+int main(){ 
+	int height; 
+cout<<"Enter your height:__\b\b"; 
+cin>>height; 
+cout<<"You are "<<height/build<<" feet and "<<height%build<<" inches high."; 
+return 0;
+}
